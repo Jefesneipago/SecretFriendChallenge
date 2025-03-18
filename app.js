8f5432c7db069c1f01ccc3eccea526a37878b6d1
Jefesneipago <38799906+Jefesneipago@users.noreply.github.com>
2025-03-18 17:39:59 -0500

    Implementación función para agregar amigos

--- app.js
+++ app.js
@@ -12,14 +12,27 @@ resultado en pantalla.
 * Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en
  "Adicionar".
 
-* Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+1. Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
 
-* Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+2. Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
 
-* Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la 
+3 . Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la 
 página.
 
 */
-function agregarAmigo(texto){
-   document.getElementById('amigo'); 
+//--------------------------1.-----------------------------
+function agregarAmigo(){
+   let nameAmigo = document.getElementById('amigo').value;//
+   
+   if (nameAmigo.trimp() === ""){
+      alert("Por favor ingrese un nombre ");
+   }else{
+      amigos.push(nameAmigo);
+      document.getElementById(amigos).value = "";
+
+      
+   }
 }
+
+//-------------------------------2.------------------------
+
